f40c46e41d15481dba3f190ad3511ef902491fc8
emeryberger <emery.berger@gmail.com>
2020-08-30 20:58:43 -0400

    Added gts; cleaned code.

--- src/excelint-cli.ts
+++ src/excelint-cli.ts
@@ -5,11 +5,11 @@
 'use strict';
 const fs = require('fs');
 const path = require('path');
-import { ExcelJSON } from './exceljson';
-import { ExcelUtils } from './excelutils';
-import { Colorize } from './colorize';
-import { Timer } from './timer';
-import { string } from 'prop-types';
+import {ExcelJSON} from './exceljson';
+import {ExcelUtils} from './excelutils';
+import {Colorize} from './colorize';
+import {Timer} from './timer';
+import {string} from 'prop-types';
 
 // Set to true to use the hard-coded example below.
 const useExample = false;
@@ -30,43 +30,76 @@ let numSheetsWithErrors = 0;
 
 // Process command-line arguments.
 const args = require('yargs')
-    .usage(usageString)
-    .command('input', 'Input from FILENAME (.xls / .xlsx file).')
-    .alias('i', 'input')
-    .nargs('input', 1)
-    .command('directory', 'Read from a directory of files (all ending in .xls / .xlsx).')
-    .alias('d', 'directory')
-    .command('formattingDiscount', 'Set discount for formatting differences (default = ' + defaultFormattingDiscount + ').')
-    .command('reportingThreshold', 'Set the threshold % for reporting anomalous formulas (default = ' + defaultReportingThreshold + ').')
-    .command('suppressOutput', 'Don\'t output the processed JSON to stdout.')
-    .command('noElapsedTime', 'Suppress elapsed time output (for regression testing).')
-    .command('maxCategories', 'Maximum number of categories for reported errors (default = ' + defaultMaxCategories + ').')
-    .command('minFixSize', 'Minimum size of a fix in number of cells (default = ' + defaultMinFixSize + ')')
-    .command('maxEntropy', 'Maximum entropy of a proposed fix (default = ' + defaultMaxEntropy + ')')
-    .command('suppressFatFix', '')
-    .command('suppressDifferentReferentCount', '')
-    .command('suppressRecurrentFormula', '')
-    .command('suppressOneExtraConstant', '')
-    .command('suppressNumberOfConstantsMismatch', '')
-    .command('suppressBothConstants', '')
-    .command('suppressOneIsAllConstants', '')
-    .command('suppressR1C1Mismatch', '')
-    .command('suppressAbsoluteRefMismatch', '')
-    .command('suppressOffAxisReference', '')
-    .command('sweep', 'Perform a parameter sweep and report the best settings overall.')
-    .help('h')
-    .alias('h', 'help')
-    .argv;
+  .usage(usageString)
+  .command('input', 'Input from FILENAME (.xls / .xlsx file).')
+  .alias('i', 'input')
+  .nargs('input', 1)
+  .command(
+    'directory',
+    'Read from a directory of files (all ending in .xls / .xlsx).'
+  )
+  .alias('d', 'directory')
+  .command(
+    'formattingDiscount',
+    'Set discount for formatting differences (default = ' +
+      defaultFormattingDiscount +
+      ').'
+  )
+  .command(
+    'reportingThreshold',
+    'Set the threshold % for reporting anomalous formulas (default = ' +
+      defaultReportingThreshold +
+      ').'
+  )
+  .command('suppressOutput', "Don't output the processed JSON to stdout.")
+  .command(
+    'noElapsedTime',
+    'Suppress elapsed time output (for regression testing).'
+  )
+  .command(
+    'maxCategories',
+    'Maximum number of categories for reported errors (default = ' +
+      defaultMaxCategories +
+      ').'
+  )
+  .command(
+    'minFixSize',
+    'Minimum size of a fix in number of cells (default = ' +
+      defaultMinFixSize +
+      ')'
+  )
+  .command(
+    'maxEntropy',
+    'Maximum entropy of a proposed fix (default = ' + defaultMaxEntropy + ')'
+  )
+  .command('suppressFatFix', '')
+  .command('suppressDifferentReferentCount', '')
+  .command('suppressRecurrentFormula', '')
+  .command('suppressOneExtraConstant', '')
+  .command('suppressNumberOfConstantsMismatch', '')
+  .command('suppressBothConstants', '')
+  .command('suppressOneIsAllConstants', '')
+  .command('suppressR1C1Mismatch', '')
+  .command('suppressAbsoluteRefMismatch', '')
+  .command('suppressOffAxisReference', '')
+  .command(
+    'sweep',
+    'Perform a parameter sweep and report the best settings overall.'
+  )
+  .help('h')
+  .alias('h', 'help').argv;
 
 if (args.help) {
-    process.exit(0);
+  process.exit(0);
 }
 
 let allFiles = [];
 
 if (args.directory) {
-    // Load up all files to process.
-    allFiles = fs.readdirSync(args.directory).filter((x: string) => x.endsWith('.xls') || x.endsWith('.xlsx'));
+  // Load up all files to process.
+  allFiles = fs
+    .readdirSync(args.directory)
+    .filter((x: string) => x.endsWith('.xls') || x.endsWith('.xlsx'));
 }
 //console.log(JSON.stringify(allFiles));
 
@@ -74,74 +107,93 @@ if (args.directory) {
 // input = filename. Default file is standard input.
 let fname = '/dev/stdin';
 if (args.input) {
-    fname = args.input;
-    allFiles = [fname];
+  fname = args.input;
+  allFiles = [fname];
 }
 
 if (!args.directory && !args.input) {
-    console.warn('Must specify either --directory or --input.');
-    process.exit(-1);
+  console.warn('Must specify either --directory or --input.');
+  process.exit(-1);
 }
 
 // argument:
 // formattingDiscount = amount of impact of formatting on fix reporting (0-100%).
 let formattingDiscount = defaultFormattingDiscount;
 if ('formattingDiscount' in args) {
-    formattingDiscount = args.formattingDiscount;
+  formattingDiscount = args.formattingDiscount;
 }
 // Ensure formatting discount is within range (0-100, inclusive).
 if (formattingDiscount < 0) {
-    formattingDiscount = 0;
+  formattingDiscount = 0;
 }
 if (formattingDiscount > 100) {
-    formattingDiscount = 100;
+  formattingDiscount = 100;
 }
 Colorize.setFormattingDiscount(formattingDiscount);
 
-if (args.suppressFatFix) { Colorize.suppressFatFix = true; }
-if (args.suppressDifferentReferentCount) { Colorize.suppressDifferentReferentCount = true; }
-if (args.suppressRecurrentFormula)  { Colorize.suppressRecurrentFormula = true; }
-if (args.suppressOneExtraConstant) { Colorize.suppressOneExtraConstant = true; }
-if (args.suppressNumberOfConstantsMismatch) { Colorize.suppressNumberOfConstantsMismatch = true; }
-if (args.suppressBothConstants) { Colorize.suppressBothConstants = true; }
-if (args.suppressOneIsAllConstants) { Colorize.suppressOneIsAllConstants = true; }
-if (args.suppressR1C1Mismatch) { Colorize.suppressR1C1Mismatch = true; }
-if (args.suppressAbsoluteRefMismatch) { Colorize.suppressAbsoluteRefMismatch = true; }
-if (args.suppressOffAxisReference) { Colorize.suppressOffAxisReference = true; }
-
+if (args.suppressFatFix) {
+  Colorize.suppressFatFix = true;
+}
+if (args.suppressDifferentReferentCount) {
+  Colorize.suppressDifferentReferentCount = true;
+}
+if (args.suppressRecurrentFormula) {
+  Colorize.suppressRecurrentFormula = true;
+}
+if (args.suppressOneExtraConstant) {
+  Colorize.suppressOneExtraConstant = true;
+}
+if (args.suppressNumberOfConstantsMismatch) {
+  Colorize.suppressNumberOfConstantsMismatch = true;
+}
+if (args.suppressBothConstants) {
+  Colorize.suppressBothConstants = true;
+}
+if (args.suppressOneIsAllConstants) {
+  Colorize.suppressOneIsAllConstants = true;
+}
+if (args.suppressR1C1Mismatch) {
+  Colorize.suppressR1C1Mismatch = true;
+}
+if (args.suppressAbsoluteRefMismatch) {
+  Colorize.suppressAbsoluteRefMismatch = true;
+}
+if (args.suppressOffAxisReference) {
+  Colorize.suppressOffAxisReference = true;
+}
 
 // As above, but for reporting threshold.
 let reportingThreshold = defaultReportingThreshold;
 if ('reportingThreshold' in args) {
-    reportingThreshold = args.reportingThreshold;
+  reportingThreshold = args.reportingThreshold;
 }
 // Ensure formatting discount is within range (0-100, inclusive).
 if (reportingThreshold < 0) {
-    reportingThreshold = 0;
+  reportingThreshold = 0;
 }
 if (reportingThreshold > 100) {
-    reportingThreshold = 100;
+  reportingThreshold = 100;
 }
 Colorize.setReportingThreshold(reportingThreshold);
 
 if ('maxCategories' in args) {
-    Colorize.maxCategories = args.maxCategories;
+  Colorize.maxCategories = args.maxCategories;
 }
 
 if ('minFixSize' in args) {
-    Colorize.minFixSize = args.minFixSize;
+  Colorize.minFixSize = args.minFixSize;
 }
 
 let maxEntropy = defaultMaxEntropy;
 if ('maxEntropy' in args) {
-    maxEntropy = args.maxEntropy;
-    // Entropy must be between 0 and 1.
-    if (maxEntropy < 0.0) {
-	maxEntropy = 0.0;
-    }
-    if (maxEntropy > 1.0) {
-	maxEntropy = 1.0;
-    }
+  maxEntropy = args.maxEntropy;
+  // Entropy must be between 0 and 1.
+  if (maxEntropy < 0.0) {
+    maxEntropy = 0.0;
+  }
+  if (maxEntropy > 1.0) {
+    maxEntropy = 1.0;
+  }
 }
 
 //
@@ -151,133 +203,160 @@ if ('maxEntropy' in args) {
 let inp = null;
 
 if (useExample) {
-    // A simple example.
-    inp = {
-        workbookName: 'example',
-        worksheets: [{
-            sheetname: 'Sheet1',
-            usedRangeAddress: 'Sheet1!E12:E21',
-            formulas: [
-                ['=D12'], ['=D13'],
-                ['=D14'], ['=D15'],
-                ['=D16'], ['=D17'],
-                ['=D18'], ['=D19'],
-                ['=D20'], ['=C21']
-            ],
-            values: [
-                ['0'], ['0'],
-                ['0'], ['0'],
-                ['0'], ['0'],
-                ['0'], ['0'],
-                ['0'], ['0']
-            ],
-            styles: [
-                [''], [''],
-                [''], [''],
-                [''], [''],
-                [''], [''],
-                [''], ['']
-            ]
-        }]
-    };
+  // A simple example.
+  inp = {
+    workbookName: 'example',
+    worksheets: [
+      {
+        sheetname: 'Sheet1',
+        usedRangeAddress: 'Sheet1!E12:E21',
+        formulas: [
+          ['=D12'],
+          ['=D13'],
+          ['=D14'],
+          ['=D15'],
+          ['=D16'],
+          ['=D17'],
+          ['=D18'],
+          ['=D19'],
+          ['=D20'],
+          ['=C21'],
+        ],
+        values: [
+          ['0'],
+          ['0'],
+          ['0'],
+          ['0'],
+          ['0'],
+          ['0'],
+          ['0'],
+          ['0'],
+          ['0'],
+          ['0'],
+        ],
+        styles: [[''], [''], [''], [''], [''], [''], [''], [''], [''], ['']],
+      },
+    ],
+  };
 }
 
 let annotated_bugs = '{}';
 try {
-    annotated_bugs = fs.readFileSync('annotations-processed.json');
-} catch (e) {
-}
+  annotated_bugs = fs.readFileSync('annotations-processed.json');
+} catch (e) {}
 
-let theBugs = JSON.parse(annotated_bugs);
+const theBugs = JSON.parse(annotated_bugs);
 
 let base = '';
 if (args.directory) {
-    base = args.directory + '/';
+  base = args.directory + '/';
 }
 
 let parameters = [];
 if (args.sweep) {
-    const step = 10;
-    for (let i = 0; i <= 100; i += step) {
-        for (let j = 0; j <= 100; j += step) {
-            parameters.push([i, j]);
-        }
+  const step = 10;
+  for (let i = 0; i <= 100; i += step) {
+    for (let j = 0; j <= 100; j += step) {
+      parameters.push([i, j]);
     }
+  }
 } else {
-    parameters = [[formattingDiscount, reportingThreshold]];
+  parameters = [[formattingDiscount, reportingThreshold]];
 }
 
-let f1scores = [];
-let outputs = [];
+const f1scores = [];
+const outputs = [];
 
-for (let parms of parameters) {
-    formattingDiscount = parms[0];
-    Colorize.setFormattingDiscount(formattingDiscount);
-    reportingThreshold = parms[1];
-    Colorize.setReportingThreshold(reportingThreshold);
+for (const parms of parameters) {
+  formattingDiscount = parms[0];
+  Colorize.setFormattingDiscount(formattingDiscount);
+  reportingThreshold = parms[1];
+  Colorize.setReportingThreshold(reportingThreshold);
 
-    let scores = [];
+  const scores = [];
 
-    for (let fname of allFiles) {
-	numWorkbooks += 1;
-        // Read from file.
-        console.warn('processing ' + fname);
-        inp = ExcelJSON.processWorkbook(base, fname);
+  for (const fname of allFiles) {
+    numWorkbooks += 1;
+    // Read from file.
+    console.warn('processing ' + fname);
+    inp = ExcelJSON.processWorkbook(base, fname);
 
-/*
+    /*
         let output = {
             'workbookName': path.basename(inp['workbookName']),
             'worksheets': {}
         };
 */
-	
-	{
-	    let hasError = false;
-	    let hasFormula = false;
-            for (let i = 0; i < inp.worksheets.length; i++) {
-		const sheet = inp.worksheets[i];
-		numSheets += 1;
-		const workbookBasename = path.basename(inp['workbookName']);
-		if (workbookBasename in theBugs) {
-		    if (sheet.sheetName in theBugs[workbookBasename]) {
-			if (theBugs[workbookBasename][sheet.sheetName]['bugs'].length > 0) {
-			    hasError = true;
-			    numSheetsWithErrors += 1;
-			}
-		    }
-		}
-		if (sheet.formulas.length > 2) { // ExceLint can't ever report an error if there are fewer than 3 formulas.
-		    hasFormula = true;
-		}
-	    }
-	    if (hasError) {
-		numWorkbooksWithErrors += 1;
-	    }
-	    if (hasFormula) {
-		numWorkbooksWithFormulas += 1;
-	    }
-	}
 
-	let output = Colorize.process_workbook(inp); // no bug processing for now
-	outputs.push(output);
+    {
+      let hasError = false;
+      let hasFormula = false;
+      for (let i = 0; i < inp.worksheets.length; i++) {
+        const sheet = inp.worksheets[i];
+        numSheets += 1;
+        const workbookBasename = path.basename(inp['workbookName']);
+        if (workbookBasename in theBugs) {
+          if (sheet.sheetName in theBugs[workbookBasename]) {
+            if (theBugs[workbookBasename][sheet.sheetName]['bugs'].length > 0) {
+              hasError = true;
+              numSheetsWithErrors += 1;
+            }
+          }
+        }
+        if (sheet.formulas.length > 2) {
+          // ExceLint can't ever report an error if there are fewer than 3 formulas.
+          hasFormula = true;
+        }
+      }
+      if (hasError) {
+        numWorkbooksWithErrors += 1;
+      }
+      if (hasFormula) {
+        numWorkbooksWithFormulas += 1;
+      }
     }
-    let averageScores = 0;
-    let sumScores = 0;
-    if (scores.length > 0) {
-        averageScores = scores.reduce((a, b) => a + b, 0) / scores.length;
-        sumScores = scores.reduce((a, b) => a + b, 0);
-    }
-    f1scores.push([formattingDiscount, reportingThreshold, sumScores]);
+
+    const output = Colorize.process_workbook(inp); // no bug processing for now
+    outputs.push(output);
+  }
+  let averageScores = 0;
+  let sumScores = 0;
+  if (scores.length > 0) {
+    averageScores = scores.reduce((a, b) => a + b, 0) / scores.length;
+    sumScores = scores.reduce((a, b) => a + b, 0);
+  }
+  f1scores.push([formattingDiscount, reportingThreshold, sumScores]);
 }
-f1scores.sort((a, b) => { if (a[2] < b[2]) { return -1; } if (a[2] > b[2]) { return 1; } return 0; });
+f1scores.sort((a, b) => {
+  if (a[2] < b[2]) {
+    return -1;
+  }
+  if (a[2] > b[2]) {
+    return 1;
+  }
+  return 0;
+});
+/*
 // Now find the lowest threshold with the highest F1 score.
-const maxScore = f1scores.reduce((a, b) => { if (a[2] > b[2]) { return a[2]; } else { return b[2]; } });
+const maxScore = f1scores.reduce((a, b) => {
+  if (a[2] > b[2]) {
+    return a[2];
+  } else {
+    return b[2];
+  }
+});
 //console.log('maxScore = ' + maxScore);
+*/
 // Find the first one with the max.
-const firstMax = f1scores.find(item => { return item[2] === maxScore; });
+/*
+const firstMax = f1scores.find(item => {
+  return item[2] === maxScore;
+});
 //console.log('first max = ' + firstMax);
+*/
+
 if (!args.suppressOutput) {
-    console.log(JSON.stringify(outputs, null, '\t'));
+  console.log(JSON.stringify(outputs, null, '\t'));
 }
 // console.log(JSON.stringify(f1scores));
 
@@ -290,7 +369,6 @@ console.log("Num sheets = " + numSheets);
 console.log("Num sheets with errors = " + numSheetsWithErrors);
 
 */
-
 
 /* disabled for now:
 console.log("Sheets with ExceLint true positives = " + sheetTruePositives);
--- src/process-annotations.js
+++ src/process-annotations.js
@@ -1,34 +1,34 @@
 'use strict';
 exports.__esModule = true;
-var fs = require('fs');
-var excelutils_1 = require("./excelutils");
-var fname = 'annotations.json';
-var content = fs.readFileSync(fname);
-var inp = JSON.parse(content);
-var out = {};
-var formulaErrors = {
-    "ref": true,
-    "refi": true,
-    "calc": true
+const fs = require('fs');
+const excelutils_1 = require('./excelutils');
+const fname = 'annotations.json';
+const content = fs.readFileSync(fname);
+const inp = JSON.parse(content);
+const out = {};
+const formulaErrors = {
+  ref: true,
+  refi: true,
+  calc: true,
 };
-for (var i = 0; i < inp.length; i++) {
-    var workbookName = inp[i]["Workbook"];
-    if (!(workbookName in out)) {
-        out[workbookName] = {};
-    }
-    var sheetName = inp[i]["Worksheet"];
-    if (!(sheetName in out[workbookName])) {
-        out[workbookName][sheetName] = {
-            "bugs": []
-        };
-    }
-    if (inp[i]["BugKind"] in formulaErrors) {
-        // Add it, converting to (row, col, ...) format.
-        var addr = inp[i]["Address"];
-        var cell_dep = excelutils_1.ExcelUtils.cell_dependency(addr, 0, 0);
-        out[workbookName][sheetName]["bugs"].push(cell_dep);
-        out[workbookName][sheetName]["bugs"].sort();
-    }
+for (let i = 0; i < inp.length; i++) {
+  const workbookName = inp[i]['Workbook'];
+  if (!(workbookName in out)) {
+    out[workbookName] = {};
+  }
+  const sheetName = inp[i]['Worksheet'];
+  if (!(sheetName in out[workbookName])) {
+    out[workbookName][sheetName] = {
+      bugs: [],
+    };
+  }
+  if (inp[i]['BugKind'] in formulaErrors) {
+    // Add it, converting to (row, col, ...) format.
+    const addr = inp[i]['Address'];
+    const cell_dep = excelutils_1.ExcelUtils.cell_dependency(addr, 0, 0);
+    out[workbookName][sheetName]['bugs'].push(cell_dep);
+    out[workbookName][sheetName]['bugs'].sort();
+  }
 }
 console.log(JSON.stringify(out, null, 2));
 //# sourceMappingURL=process-annotations.js.map
